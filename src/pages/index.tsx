1b72ba4e7aa946b0d27ebbccb4ab6cfbe8959e72
Faraz ud din <youremail@example.com>
2023-05-20 16:55:15 +0500

    Dashboard added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,7 +53,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]" id="DBPage">
         {/* warning message */}
 
         {showWarning && (
