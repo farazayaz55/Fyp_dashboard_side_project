2d84d2829530e5336f6a93a1c2828864cbf210e5
Faraz ud din <youremail@example.com>
2023-05-20 13:58:56 +0500

    First commit

---
+++ src/Components/alert.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+
+interface AlertProps {
+    showWarning: boolean;
+    errorMessage: string;
+    setShowWarning: React.Dispatch<React.SetStateAction<boolean>>;
+    setErrorMessage: React.Dispatch<React.SetStateAction<string>>;
+  }
+  
+
+const Alert: React.FC<AlertProps> = ({
+    showWarning,
+    errorMessage,
+    setShowWarning,
+    setErrorMessage
+}) => {
+
+      //to hide warning message after 5 seconds
+  useEffect(() => {
+    if (showWarning) {
+      const timeoutId = setTimeout(() => {
+        setShowWarning(false);
+      }, 2000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [showWarning]);
+  return (
+    <div className="absolute right-4 top-4">
+            <div className="alert alert-warning shadow-lg">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 flex-shrink-0 stroke-current"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                  />
+                </svg>
+                <span>{errorMessage}</span>
+              </div>
+            </div>
+          </div>
+  );
+};
+
+export default Alert;
---
+++ src/data/data.tsx
@@ -0,0 +1,272 @@
+export const data=[
+    {
+      "id": "japan",
+      "color": "hsl(312, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 5
+        },
+        {
+          "x": "helicopter",
+          "y": 220
+        },
+        {
+          "x": "boat",
+          "y": 241
+        },
+        {
+          "x": "train",
+          "y": 129
+        },
+        {
+          "x": "subway",
+          "y": 72
+        },
+        {
+          "x": "bus",
+          "y": 227
+        },
+        {
+          "x": "car",
+          "y": 187
+        },
+        {
+          "x": "moto",
+          "y": 36
+        },
+        {
+          "x": "bicycle",
+          "y": 6
+        },
+        {
+          "x": "horse",
+          "y": 41
+        },
+        {
+          "x": "skateboard",
+          "y": 128
+        },
+        {
+          "x": "others",
+          "y": 90
+        }
+      ]
+    },
+    {
+      "id": "france",
+      "color": "hsl(300, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 150
+        },
+        {
+          "x": "helicopter",
+          "y": 75
+        },
+        {
+          "x": "boat",
+          "y": 103
+        },
+        {
+          "x": "train",
+          "y": 134
+        },
+        {
+          "x": "subway",
+          "y": 290
+        },
+        {
+          "x": "bus",
+          "y": 265
+        },
+        {
+          "x": "car",
+          "y": 283
+        },
+        {
+          "x": "moto",
+          "y": 254
+        },
+        {
+          "x": "bicycle",
+          "y": 66
+        },
+        {
+          "x": "horse",
+          "y": 222
+        },
+        {
+          "x": "skateboard",
+          "y": 48
+        },
+        {
+          "x": "others",
+          "y": 74
+        }
+      ]
+    },
+    {
+      "id": "us",
+      "color": "hsl(332, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 260
+        },
+        {
+          "x": "helicopter",
+          "y": 264
+        },
+        {
+          "x": "boat",
+          "y": 210
+        },
+        {
+          "x": "train",
+          "y": 56
+        },
+        {
+          "x": "subway",
+          "y": 284
+        },
+        {
+          "x": "bus",
+          "y": 277
+        },
+        {
+          "x": "car",
+          "y": 288
+        },
+        {
+          "x": "moto",
+          "y": 159
+        },
+        {
+          "x": "bicycle",
+          "y": 99
+        },
+        {
+          "x": "horse",
+          "y": 161
+        },
+        {
+          "x": "skateboard",
+          "y": 0
+        },
+        {
+          "x": "others",
+          "y": 273
+        }
+      ]
+    },
+    {
+      "id": "germany",
+      "color": "hsl(288, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 126
+        },
+        {
+          "x": "helicopter",
+          "y": 140
+        },
+        {
+          "x": "boat",
+          "y": 37
+        },
+        {
+          "x": "train",
+          "y": 0
+        },
+        {
+          "x": "subway",
+          "y": 159
+        },
+        {
+          "x": "bus",
+          "y": 235
+        },
+        {
+          "x": "car",
+          "y": 243
+        },
+        {
+          "x": "moto",
+          "y": 25
+        },
+        {
+          "x": "bicycle",
+          "y": 58
+        },
+        {
+          "x": "horse",
+          "y": 66
+        },
+        {
+          "x": "skateboard",
+          "y": 135
+        },
+        {
+          "x": "others",
+          "y": 222
+        }
+      ]
+    },
+    {
+      "id": "norway",
+      "color": "hsl(272, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 32
+        },
+        {
+          "x": "helicopter",
+          "y": 79
+        },
+        {
+          "x": "boat",
+          "y": 172
+        },
+        {
+          "x": "train",
+          "y": 77
+        },
+        {
+          "x": "subway",
+          "y": 187
+        },
+        {
+          "x": "bus",
+          "y": 201
+        },
+        {
+          "x": "car",
+          "y": 34
+        },
+        {
+          "x": "moto",
+          "y": 243
+        },
+        {
+          "x": "bicycle",
+          "y": 81
+        },
+        {
+          "x": "horse",
+          "y": 185
+        },
+        {
+          "x": "skateboard",
+          "y": 282
+        },
+        {
+          "x": "others",
+          "y": 136
+        }
+      ]
+    }
+  ]
